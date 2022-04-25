063d93131ed3aaf6fb89d491fd48bc180857fbde
Juned <87689793+mafiacoders@users.noreply.github.com>
2022-04-25 17:12:18 +0530

    push

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,26 +1,41 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
 export const Login = () => {
   //  use reqres to log user in.
-  const [user, setUser] = useState({username:"", password:""});
-    const {handleAuth, isAuth, setIsAuth} = useContext(AuthContext);
-    const [token, setToken] = useState("");
-
-    const handleChange = (e)=>{
-        let {name, value} = e.target;
-        setUser({...user, [name]:value});
+  const [user, setUser] = useState({ username: "", password: "" });
+  const { handleAuth, isAuth, setIsAuth } = useContext(AuthContext);
+ const navigate = useNavigate();
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setUser({
+      ...user,
+      [e.target.name]: value,
+    });
+  };
+  const pressH = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post("https://reqres.in/api/login", user);
+      if (res.status !== 200) {
+        throw new Error("Error logging in");
+      }
+      handleAuth(true);
+      navigate(-2, { replace: true });
+    } catch (er) {
+      alert(er.message);
     }
+  };
 
   return (
-    <form className="loginform">
+    <form className="loginform" onSubmit={pressH}>
       <input
         name="username"
         type="text"
         placeholder="Enter username"
         className="login_username"
         onChange={handleChange}
-
       />
       <input
         name="password"
